5103e1f9cc9e149df8a5855af969656ac2a0d3bc
S202402731 <jyj177100@naver.com>
2025-10-06 16:04:43 +0900

    과제제출

--- student.cpp
+++ student.cpp
@@ -6,29 +6,36 @@
 
 StudentRecord::StudentRecord(float midterm, float final) 
 /* TODO: Initializer list 사용해 초기화 해보기 */
-{}
+    : midterm(midterm), final(final)
+{ }
 
 float StudentRecord::getTotal() const {
     /* TODO */
-    return 0.0f;
+    return (midterm + final) / 2.0f;
 }
 
 void StudentRecord::updateScores(float midterm, float final) {
     /* TODO: this keyword 사용해 보기 */
+    this->midterm = midterm;
+    this->final = final;
 }
 
 
 /* Student */
+int Student::last_student_id = 1000;
 
 Student::Student() 
 /* TODO: Initializer list 사용해 초기화 해보기 */
+    : name(""), id(-1), record(0.0f, 0.0f)
 { }
 
 Student::Student(const char *name, int id, float midterm, float final)
 /* TODO: Initializer list 사용해 초기화 해보기 */
+    : name(name), id(id), record(midterm, final)
 {  }
 
 void Student::updateRecord(const char *name, float midterm, float final) {
     /* TODO */
+    this->name = name;
+    this->record.updateScores(midterm, final);
 }
-
--- student_manager.cpp
+++ student_manager.cpp
@@ -5,51 +5,102 @@
 int StudentManager::last_student_id = 1000;
 
 void StudentManager::addStudent(char const* name, float midterm, float final) {
+    if (num_of_students >= MAX_STUDENTS) {
+        std::cout << "Error: Cannot add more students.\n";
+        return;
+    }
     int id = ++last_student_id;
     /* TODO */
-
+    students[num_of_students++] = Student(name, id, midterm, final);
 }
 
 void StudentManager::deleteStudent(int id) {
     /* TODO */
+    int idx = findStudentByID(id);
+    if (idx == -1) return;
+    for (int i = idx; i < num_of_students - 1; ++i)
+        students[i] = students[i + 1];
+    num_of_students--;
 }
 
 void StudentManager::modifyStudent(const Student& student) {
     /* TODO */
+    int idx = findStudentByID(student.getID());
+    if (idx == -1) return;
+    students[idx] = student;
 }
 
 int StudentManager::findStudentByID(int id) const {
     /* TODO */
+    for (int i = 0; i < num_of_students; ++i)
+        if (students[i].getID() == id)
+            return i;
     return -1;
 }
 
 int StudentManager::findBestStudentInMidterm() const {
     /* TODO */
-    return -1;
+    if (num_of_students == 0) return -1;
+    int bestIdx = 0;
+    float best = students[0].getRecord().getMidterm();
+    for (int i = 1; i < num_of_students; ++i)
+        if (students[i].getRecord().getMidterm() > best) {
+            best = students[i].getRecord().getMidterm();
+            bestIdx = i;
+        }
+    return students[bestIdx].getID();
 }
 
 int StudentManager::findBestStudentInFinal() const {
     /* TODO */
-    return -1;
+    if (num_of_students == 0) return -1;
+    int bestIdx = 0;
+    float best = students[0].getRecord().getFinal();
+    for (int i = 1; i < num_of_students; ++i)
+        if (students[i].getRecord().getFinal() > best) {
+            best = students[i].getRecord().getFinal();
+            bestIdx = i;
+        }
+    return students[bestIdx].getID();
 }
 
 int StudentManager::findBestStudent() const {
     /* TODO */
-    return -1;
+    if (num_of_students == 0) return -1;
+    int bestIdx = 0;
+    float best = students[0].getRecord().getTotal();
+    for (int i = 1; i < num_of_students; ++i)
+        if (students[i].getRecord().getTotal() > best) {
+            best = students[i].getRecord().getTotal();
+            bestIdx = i;
+        }
+    return students[bestIdx].getID();
 }
 
 float StudentManager::getMidtermAverage() const {
     /* TODO */
-    return 0.0f;
+    if (num_of_students == 0) return 0.0f;
+    float sum = 0;
+    for (int i = 0; i < num_of_students; ++i)
+        sum += students[i].getRecord().getMidterm();
+    return sum / num_of_students;
 }
 
 float StudentManager::getFinalAverage() const {
     /* TODO */
-    return 0.0f;
+    if (num_of_students == 0) return 0.0f;
+    float sum = 0;
+    for (int i = 0; i < num_of_students; ++i)
+        sum += students[i].getRecord().getFinal();
+    return sum / num_of_students;
 }
 
 float StudentManager::getTotalAverage() const {
     /* TODO */
-    return 0.0f;
+    if (num_of_students == 0) return 0.0f;
+    float sum = 0;
+    for (int i = 0; i < num_of_students; ++i)
+        sum += students[i].getRecord().getTotal();
+    return sum / num_of_students;
 }
 
